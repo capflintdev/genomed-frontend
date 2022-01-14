16690d3f82223bd5240b5fb94df3df1678c4a26b
Kseniya Antipina <karinpark@mail.ru>
2022-01-14 11:34:40 +0300

    added yandex metrika

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,13 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document';
+import ym from 'react-yandex-metrika';
+import { YMInitializer } from 'react-yandex-metrika';
+import Router from 'next/router';
+
+Router.events.on('routeChangeComplete', (url: string) => {
+    if (typeof window !== 'undefined') {
+        ym('hit', url);
+    }
+});
 
 class MyDocument extends Document {
     static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
@@ -11,8 +20,14 @@ class MyDocument extends Document {
             <Html lang="ru">
                 <Head>
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link rel="preconnect" href="https://mc.yandex.ru" />
                     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:wght@300;400;500;600;700&family=Raleway:wght@400;500;600&display=swap" rel="stylesheet" />
                 </Head>
+                <YMInitializer
+                    accounts={[87143214]}
+                    options={{ webvisor: true, defer: true, clickmap: true, trackLinks: true, accurateTrackBounce: true }}
+                    version="2"
+                />
                 <body>
                     <Main />
                     <NextScript />
